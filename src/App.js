48d90deae6614ff57b79a722d76856473b077df6
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-10-04 14:13:29 +1100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Lisa Wolf and is open-sourced with the code being found at{" "}
+        <a
+          href="https://github.com/LadyDarkWolf/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
-
-export default App;

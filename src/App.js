e769177f64438cb0dd338371abe538503a1fc405
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-10-04 15:02:23 +1100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by Lisa Wolf and is open-sourced with the code being found at{" "}
-        <a
-          href="https://github.com/LadyDarkWolf/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello world</button>
+        <footer>
+          Coded by Lisa Wolf and is open-sourced with the code being found at{" "}
+          <a
+            href="https://github.com/LadyDarkWolf/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }

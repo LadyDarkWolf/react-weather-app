dcddecd47f90288b73ae923b2dfeaff479a50f1f
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-10-05 18:26:56 +1100

    Added search functionality. Moved the weather display into its own compoment.

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FriendlyDate from "./FriendlyDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     setWeatherData({
@@ -18,10 +19,24 @@ export default function Weather(props) {
       ready: true,
     });
   }
+
+  function search() {
+    const apiKey = "2af1ff2de81cdd8d67552da7d4b4331d";
+    let apiBaseUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&units=metric&q=${city}`;
+    axios.get(apiBaseUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Search for entered city
+    search();
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-md-9">
               <input
@@ -29,6 +44,7 @@ export default function Weather(props) {
                 placeholder="Enter a city"
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-md-3">
@@ -40,36 +56,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FriendlyDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-md-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            <span className="align-top temperature align-top">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="units align-top">°C</span>
-          </div>
-
-          <div className="col-md-6">
-            <ul>
-              <li>Precipitation: {weatherData.precipitation}%</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h </li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "2af1ff2de81cdd8d67552da7d4b4331d";
-    let apiBaseUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&units=metric&q=${props.defaultCity}`;
-    axios.get(apiBaseUrl).then(handleResponse);
+    search();
     return "Loading ...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import FriendlyDate from "./FriendlyDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FriendlyDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-md-6">
+          <img src={props.data.iconUrl} alt={props.data.description} />
+          <span className="align-top temperature align-top">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="units align-top">°C</span>
+        </div>
+
+        <div className="col-md-6">
+          <ul>
+            <li>Precipitation: {props.data.precipitation}%</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
